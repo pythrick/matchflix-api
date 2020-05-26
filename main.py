8788815ce74f2c8777c78d8072f0f885ab58acd4
Pythrick <patrhick@gmail.com>
2020-05-26 00:38:23 -0300

    second commit

--- main.py
+++ main.py
@@ -1,48 +1,60 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import List
 from uuid import UUID
+import dataset
 import enum
 
 app = FastAPI()
 
 
-class Swipe(enum.Enum):
+class Action(enum.Enum):
     LEFT = "LEFT"
     RIGHT = "RIGHT"
+    DID_NOT_WATCH = "DID_NOT_WATCH"
+
+
+class Answer(BaseModel):
+    release_id: int
+    action: Action
 
 
 class UserAnswer(BaseModel):
-    release_id: UUID
-    swipe: Swipe
+    email: str
+    answers: List[Answer]
 
 
 @app.get("/releases")
 def list_releases():
-    return [
+    db = dataset.connect('sqlite:///db.sqlite3')
+    result = db.query('SELECT * FROM releases LIMIT 50')
+    result = [
         {
-            "cover": "",
-            "title": "",
-            "id": ""
-        },
-        {
-            "cover": "",
-            "title": "",
-            "id": ""
-        },
-        {
-            "cover": "",
-            "title": "",
-            "id": ""
-        },
-        {
-            "cover": "",
-            "title": "",
-            "id": ""
+            "cover": r["image"],
+            "title": r["title"],
+            "id": r["id"],
+            "description": r["description"]
         }
+        for r in result
     ]
+    return result 
 
 
-@app.post("/users-answers")
+@app.post("/users-answers", status_code=201)
 def create_user_answers(user_answer: UserAnswer):
-    #TODO: Store on database
-    return user_answer
+    db = dataset.connect('sqlite:///db.sqlite3')
+    user_table = db["user"]
+
+    user_id = user_table.upsert(dict(email=user_answer.email), ["email"])
+    answers_table = db["answers"]
+
+    for answer in user_answer.answers:
+        answers_table.insert({
+            "release_id": answer.release_id,
+            "action": answer.action.value,
+            "user_id": user_id
+        })
+
+    return {
+        "message": "Success!"
+    }
