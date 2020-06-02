8f91df056c5f18a71272e362e589541ae83046f9
Pythrick <patrhick@gmail.com>
2020-06-01 23:23:31 -0300

    Adiciona CORS na parada

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from typing import List
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pprint import pprint
 
 from matchflix.extensions.db import database
 from matchflix.models import Movie, UserMovies
 from matchflix import usecases
 
+
 app = FastAPI()
+
+origins = [
+    "http://localhost:8080",
+    "http://0.0.0.0:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
@@ -28,6 +43,6 @@ async def list_movies():
 @app.post("/user-movies", status_code=201)
 async def create_user_movies(user_movies: UserMovies):
     user_id = await usecases.register_user_movies(user_movies)
-    matched_user, score = await usecases.find_match(user_id)
-    movie = usecases.recommend_movie(user_id, matched_user["id"])
-    pprint(movie)
+    # matched_user, score = await usecases.find_match(user_id)
+    # movie = usecases.recommend_movie(user_id, matched_user["id"])
+    # pprint(movie)
